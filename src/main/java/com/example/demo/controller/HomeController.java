fedcdf3f840b2094e39f6676018ff4e1478ba51c
KorakotPT <korakot.pr@kkumail.com>
2026-08-09 20:22:04 +0700

    Complete Lab06 quiz practical tasks

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,14 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Hello from Thymeleaf with a custom ViewResolver!");
-        return "home"; // คืนค่าเป็นชื่อ view เชิงตรรกะ
+        model.addAttribute("message", "สวัสดี นายกรกฏ พรมทอง");
+        model.addAttribute("studentId", "673380025-8");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("info", "สวัสดี ผมชื่อ นายกรกฏ พรมทอง รหัสนักศึกษา 673380025-8 ชื่อเล่น บิ๊ก อายุ 20 เกิด 26 กันยายน 2569 กำลังศึกษาวิชา CP353002 Principles of Software Design");
+        return "about";
     }
 }
